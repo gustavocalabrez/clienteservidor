43434c990e0b7897982cdccd291f875dfb9cf0d5
gustavocalabrez <70350151+gustavocalabrez@users.noreply.github.com>
2020-08-27 20:27:35 -0300

    Update server.py

--- server.py
+++ server.py
@@ -167,6 +167,7 @@ def update_usuario(user):
 
 if __name__ == '__main__':
     
-    app.run(use_reloader=False,debug=True,port=33507)
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port, use_reloader=False)
     
 
