9e8b1f01976832ee42936328c152f6b1361ebec6
gustavocalabrez <70350151+gustavocalabrez@users.noreply.github.com>
2020-09-28 19:09:40 -0300

    Update server.py

--- server.py
+++ server.py
@@ -142,10 +142,8 @@ def get_ocurrence(user, role, ocurrence_id):
         return make_response('', 200)
     elif request.method == 'DELETE':
         i = request.get_json()
-        print(i)
-        #user_data = ocorrencia.query.filter_by(_id = i['_id']).first()
-        #db.session.delete(user_data)
-        #db.session.commit()
+        ocorrencia.query.filter_by(_id = i['_id']).delete()
+        db.session.commit()
         return make_response('', 200)
 @app.route('/ocurrences/me', methods=['GET'])
 @token_required
