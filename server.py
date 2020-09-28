313cabcd9a2a2570e5d018e7984f598e114258ee
gustavocalabrez <70350151+gustavocalabrez@users.noreply.github.com>
2020-09-28 18:13:54 -0300

    Update server.py

--- server.py
+++ server.py
@@ -99,7 +99,7 @@ def get_ocurrence(user, role, ocurrence_id):
 @token_required
 def get_ocurrence_me(user, role):
     print(user.email)
-    ocorrencias = ocorrencia.query.filter_by(user_id = user._id]).all()
+    ocorrencias = ocorrencia.query.filter_by(user_id = user._id).all()
     if not ocorrencias:
         return make_response('Não temos ocorrencias com este usuario', 404)
     for i in ocorrencias:
